6d90793e3066bb6f8ba2b040197801bb0141d0be
Farkas80 <csaba.farkas80@gmail.com>
2021-06-04 07:07:52 +0200

    Add js first steps

---
+++ stopwatch.js
@@ -0,0 +1,15 @@
+console.log('hello');
+
+let seconds = 00;
+let tens = 00;
+let appendSeconds = document.getElementsByClassName('seconds');
+let appendTens = document.getElementsByClassName('tens');
+let startBtn = document.getElementsByClassName('start');
+let stopBtn = document.getElementsByClassName('stop');
+let resetBtn = document.getElementsByClassName('reset');
+
+console.log(appendSeconds);
+console.log(appendTens);
+console.log(startBtn);
+console.log(stopBtn);
+console.log(resetBtn);

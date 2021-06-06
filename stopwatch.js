be6e4f7ac502f37dd4c2bf3ee963daf5081d56d1
Farkas80 <csaba.farkas80@gmail.com>
2021-06-06 21:27:35 +0200

    Update

--- stopwatch.js
+++ stopwatch.js
@@ -1,57 +1,60 @@
-window.onload = function() {
-
-let seconds = 00;
-let tens = 00;
-let appendSeconds = document.getElementsByClassName('seconds');
-let appendTens = document.getElementsByClassName('tens');
-let startBtn = document.getElementsByClassName('start');
-let stopBtn = document.getElementsByClassName('stop');
-let resetBtn = document.getElementsByClassName('reset');
-let interval;
-
-console.log(appendSeconds);
-console.log(appendTens);
-console.log(startBtn);
-console.log(stopBtn);
-console.log(resetBtn);
-
-startBtn.onclick = function() {
-    clearInterval(interval);
-    interval = setInterval(startTimer, 10);
-};
-
-stopBtn.onclick = function() {
-    clearInterval(interval);
-};
-
-resetBtn.onclick = function() {
-    clearInterval(interval);
-    seconds = '00';
-    tens = '00';
-    appendSeconds.innerHTML = seconds;
-    appendTens.innerHTML = tens;
-};
-
-function startTimer () {
-    tens++;
-    if(tens <= 9) {
-        appendTens.innerHTML = '0' + tens;
+window.onload = function () {
+  
+    let seconds = 00; 
+    let tens = 00; 
+    let appendTens = document.getElementById("tens")
+    let appendSeconds = document.getElementById("seconds")
+    let buttonStart = document.getElementById('button-start');
+    let buttonStop = document.getElementById('button-stop');
+    let buttonReset = document.getElementById('button-reset');
+    let Interval;
+  
+    buttonStart.onclick = function() {
+      
+       clearInterval(Interval);
+       Interval = setInterval(startTimer, 10);
     }
-
-    if(tens > 9) {
-        appendTens.innerHTML = tens;
+    
+      buttonStop.onclick = function() {
+         clearInterval(Interval);
     }
-
-    if(tens >99) {
-        console.log('seconds');
-        seconds++;
-        appendSeconds.innerHTML = '0' + seconds;
-        tens = 0;
-        appendTens.innerHTML = ('0' + 0);
-    }
-
-    if(seconds > 9) {
+    
+  
+    buttonReset.onclick = function() {
+       clearInterval(Interval);
+      tens = "00";
+        seconds = "00";
+      appendTens.innerHTML = tens;
         appendSeconds.innerHTML = seconds;
     }
-}
-}
+    
+     
+    
+    function startTimer () {
+      tens++; 
+      
+      if(tens <= 9){
+        appendTens.innerHTML = "0" + tens;
+      }
+      
+      if (tens > 9){
+        appendTens.innerHTML = tens;
+        
+      } 
+      
+      if (tens > 99) {
+        console.log("seconds");
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      
+      if (seconds > 9){
+        appendSeconds.innerHTML = seconds;
+      }
+    
+    }
+    
+  
+  }
